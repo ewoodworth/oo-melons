bed33ed731c1f803093c6faeac46b128b432c7c6
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-06 17:09:03 -0700

    Completed part one of exercises

--- melons.py
+++ melons.py
@@ -11,6 +11,7 @@ class AbstractMelonOrder(object):
         self.shipped = False
         self.order_type = None
         self.tax = None
+        self.country_code = country_code
 
     def get_total(self):
         """Calculate price."""
@@ -29,57 +30,22 @@ class AbstractMelonOrder(object):
 
         return self.country_code
 
-class DomesticMelonOrder(object):
+class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
-    def __init__(self, species, qty):
-        """Initialize melon order attributes"""
+    def __init__(self, species, quantity):
+        """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
-        self.shipped = False
+        super(DomesticMelonOrder, self).__init__(species, quantity, 'USA')
         self.order_type = "domestic"
         self.tax = 0.08
 
-    def get_total(self):
-        """Calculate price."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
-
-    def mark_shipped(self):
-        """Set shipped to true."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder(object):
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
-    def __init__(self, species, qty, country_code):
+    def __init__(self, species, quantity, country_code):
         """Initialize melon order attributes"""
 
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
+        super(InternationalMelonOrder, self).__init__(species, quantity, country_code)
         self.order_type = "international"
         self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
-
-    def mark_shipped(self):
-        """Set shipped to true."""
-
-        self.shipped = True
-
-    def get_country_code(self):
-        """Return the country code."""
-
-        return self.country_code
